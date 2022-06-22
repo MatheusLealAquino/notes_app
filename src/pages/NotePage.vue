5acccecf5f173a1a4ffafb9de766f6b4b8408d72
Matheus Leal <matheusaquino@id.uff.br>
2022-06-21 22:04:17 -0300

    feat: increase page with note

--- src/pages/NotePage.vue
+++ src/pages/NotePage.vue
@@ -6,7 +6,53 @@
       </template>
     </q-input>
 
-    <q-editor v-model="note" min-height="20rem" min-width="100%" />
+    <q-editor
+      v-model="note"
+      min-height="20rem"
+      width="100%"
+      :toolbar="[
+        [
+          {
+            label: $q.lang.editor.align,
+            icon: $q.iconSet.editor.align,
+            fixedLabel: true,
+            list: 'only-icons',
+            options: ['left', 'center', 'right', 'justify'],
+          },
+        ],
+        ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+        ['token', 'hr', 'link', 'custom_btn'],
+        ['print', 'fullscreen'],
+        [
+          {
+            label: $q.lang.editor.formatting,
+            icon: $q.iconSet.editor.formatting,
+            list: 'no-icons',
+            options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
+          },
+          {
+            label: $q.lang.editor.fontSize,
+            icon: $q.iconSet.editor.fontSize,
+            fixedLabel: true,
+            fixedIcon: true,
+            list: 'no-icons',
+            options: [
+              'size-1',
+              'size-2',
+              'size-3',
+              'size-4',
+              'size-5',
+              'size-6',
+              'size-7',
+            ],
+          },
+          'removeFormat',
+        ],
+        ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+        ['undo', 'redo'],
+        ['viewsource'],
+      ]"
+    />
 
     <q-dialog v-model="modalOpen" persistent>
       <q-card style="min-width: 350px">
